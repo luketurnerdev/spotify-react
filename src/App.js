51e48d407626b7e81777d3583a5f7904f0939c49
luketurnerdev <luke@lukedturner.com>
2020-02-26 14:57:02 +1100

    add react router for navigation'

--- src/App.js
+++ src/App.js
@@ -1,34 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import TopArtists from "./Components/TopArtists.jsx"
-import Login from "./Components/Login.jsx"
-import Dashboard from "./Components/Dashboard";
+import Routes from "./Components/Routes";
 import './App.css';
 import axios from 'axios';
-
-function LoggedStatus(props) {
-  const isLoggedIn = props.isLoggedIn;
-
-  if (isLoggedIn) {
-    return <Dashboard />
-  } 
-  return <Login />
-}
 
 function App(props) {
 
   return (
     <>
-      <LoggedStatus isLoggedIn= {true} />
+         <Routes/>
     </>
   )
 }
-
-
-
-
-
-// ReactDOM.render(<TopArtists/>, document.getElementById('root'));
 
 
 export default App;
