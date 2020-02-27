3464f932c6c1fe40a6e004fc3ed604bbb9d61414
luketurnerdev <luke@lukedturner.com>
2020-02-27 16:58:14 +1100

    testing redirects, backend blocked by cors

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, {Component, useState} from 'react';
 import axios from 'axios';
 
 import {Typography, Grid, Button} from '@material-ui/core'
@@ -6,21 +6,22 @@ import {Typography, Grid, Button} from '@material-ui/core'
 const spotifyLoginRedirect = () => {
     //  Get request to {backend_url}/auth/login
     // Are you sure? If yes, then...
-
+    
     let url = "";
     axios.get(`${process.env.REACT_APP_BACKEND_API}/auth/login`)
     .then(resp => {
-        url = resp.data;
+        // url = resp.data;
         console.log(url);
     })
     .catch(err => {
         // alert(err);
         console.log(err);
     })
-    window.location.href = url;
+    // window.location.href = url;
 }
 
 const Login = () => {
+    const [loggedIn, setLoggedIn] = useState(false);
     return (
         <div>
             <Button variant="contained"
