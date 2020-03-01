20ac8cd7bb72aed054c541858d2568081f69b24d
Luke Turner <luke@lukedturner.com>
2020-03-01 11:40:05 +1100

    useState example

--- src/App.js
+++ src/App.js
@@ -3,14 +3,36 @@ import Routes from "./Components/Routes";
 import './App.css';
 import axios from 'axios';
 
-function App(props) {
+const UserContext = React.createContext({
+  userID: null,
+  accessToken: null,
+})
 
+function App(props) {
+  
+  const [theme, setTheme] = React.useState("red");
+  
+  const onClickHandler = () => {
+    setTheme(theme === "red" ? "blue" : "red")
+  }
   return (
     <>
+        <Text theme={theme}/>
+          <button onClick={onClickHandler}>Theme</button>
+
+        
          <Routes/>
     </>
   )
 }
+
+function Text({theme}) {
+  return(
+    <h1 style = {{
+       color: `${theme}`
+    }}>{theme}</h1>
+  );
+  }
 
 
 export default App;
