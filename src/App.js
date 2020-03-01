b285bb3ab694a05f7f04ea712beafc8797ca1e0e
Luke Turner <luke@lukedturner.com>
2020-03-01 12:51:59 +1100

    successfully grabbed user playlists and logged them

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import Routes from "./Components/Routes";
 import UserContext from "./UserContext";
-import Main from "./Main";
 import Cookies from 'js-cookie';
 import './App.css';
 import axios from 'axios';
 
 function App() {
   
-  const theme = "light";
 
   return (
     <>
@@ -16,23 +14,12 @@ function App() {
           userID: Cookies.get('userID'),
           accessToken: Cookies.get('accessToken'),
         }}>
-          <div>
-            <Main/>
-          </div>
+         <Routes/>
         </UserContext.Provider>
         
-         <Routes/>
     </>
   )
 }
-
-function Text({theme}) {
-  return(
-    <h1 style = {{
-       color: `${theme}`
-    }}>{theme}</h1>
-  );
-  }
 
 
 export default App;
--- src/pages/Playlists/index.jsx
+++ src/pages/Playlists/index.jsx
@@ -1,11 +1,20 @@
-import React, {Component} from 'react';
+import React, {Component, useContext, useState, useEffect} from 'react';
+import UserContext from "./../../UserContext";
 import PlaylistCard from "../../Components/PlaylistCard";
 import styles from "./styles";
+import axios from "axios";
 
 import {Typography, Grid} from '@material-ui/core'
 
 const Playlists = () => {
 
+  
+  const userInfo = useContext(UserContext);
+  const [userPlaylists, setUserPlaylists] = useState([]);
+
+  useEffect(() => {
+    getUserPlaylists();
+  });
   const testPlaylistObject = [
     {title: "First one", trackAmount: 5},
     {title: "Second one", trackAmount: 8},
@@ -14,11 +23,34 @@ const Playlists = () => {
     {title: "Fifth one", trackAmount: 4},
     {title: "Sixth one", trackAmount: 4},
   ]
-  
-  return (
-    <div>
-      <Typography variant="h3">Gday!</Typography>
 
+  async function getUserPlaylists (req, res) {
+    // console.log(res)
+    let token = userInfo.accessToken || null;
+    let userID = userInfo.userID || null;
+    let playlists = axios({
+        method: 'GET',
+        url: `https://api.spotify.com/v1/users/${userID}/playlists`,
+        headers: {
+            "Authorization": `Bearer ${token}`,
+            "Content-Type": "application/json",
+        },
+    })
+    .then(resp => {
+        // return resp.data.items;
+        console.log(resp.data)
+        res.json(resp.data.items);
+    })
+    .catch(err => {
+      // IF IT COMES BACK WITH 'invalid token, prompt the re login
+        console.log(err);
+    })
+
+    return token;
+}
+
+  const PlaylistsContainer = () => {
+    return(
       <div className="playlistsContainer">
         {/* //Dynamically map here later */}
         <Grid
@@ -40,7 +72,14 @@ const Playlists = () => {
         })}
         </Grid>
       </div>
-
+    )
+  }
+  
+  return (
+    <div>
+      <Typography variant="h3">token: {userInfo.accessToken}</Typography>
+      {/* <h1> Playlists: {userPlaylists[0]}</h1> */}
+    
     </div>
     )
 }
