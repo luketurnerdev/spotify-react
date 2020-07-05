0189a5905dfd77a6abebc9ef5ee9648ae159fdb3
luketurnerdev <luke@lukedturner.com>
2020-07-05 21:41:06 +1000

    add user iamge

--- src/Components/Main/index.js
+++ src/Components/Main/index.js
@@ -10,7 +10,7 @@ const Main = props => {
         console.log('testclick')
         const playlists = await axios({
             method: 'GET',
-            url: `https://api.spotify.com/v1/users/${userData.id}/playlists`,
+            url: `https://api.spotify.com/v1/users/${userData.id}/playlists?limit=50`,
             headers: {
                 "Authorization": `Bearer ${token}`,
                 "Content-Type": "application/json",
@@ -29,7 +29,8 @@ const Main = props => {
     return (
         <>
             <h1>{userData.display_name ? `Logged in as ${userData.display_name}` : 'Welcome'} </h1>
-            <Button onClick={testAPICall}>Clickola</Button>
+            <Button onClick={testAPICall}>Get Playlists (shown in console)</Button>
+            {userData.images && <img src={userData.images[0].url} alt="User profile" style={{height:'100px', width: 'auto'}} />}
         </>
     )
 };
